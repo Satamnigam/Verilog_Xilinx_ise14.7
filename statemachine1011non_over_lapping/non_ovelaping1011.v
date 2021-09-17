552cf405257727e96724150c38e31ce6a723a055
Satamnigam <64124488+Satamnigam@users.noreply.github.com>
2021-09-18 01:39:11 +0530

    Add files via upload

---
+++ statemachine1011non_over_lapping/non_ovelaping1011.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:33:41 07/01/2019 
+// Design Name: 
+// Module Name:    non_ovelaping1011 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module statemachine(
+    input clk,
+    input x,
+	 output reg y  ,
+	 output [1:0]s
+    );
+	 reg [1:0] s;
+initial {s,y} =3'b000;
+always @(negedge clk)
+begin
+case({s,x})
+3'b000: {s,y} = 3'b000;
+3'b001: {s,y} = 3'b010;
+3'b010: {s,y} = 3'b100;
+3'b011: {s,y} = 3'b000;
+3'b100: {s,y} = 3'b000;
+3'b101: {s,y} = 3'b110;
+3'b110: {s,y} = 3'b000;
+3'b111: {s,y} = 3'b001;
+endcase
+end
+endmodule
---
+++ statemachine1011non_over_lapping/nonoverlapping_test.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:37:19 07/01/2019
+// Design Name:   statemachine
+// Module Name:   C:/Users/satyam/Desktop/my codes/statemachine1011non_over_lapping/nonoverlapping_test.v
+// Project Name:  statemachine1011non_over_lapping
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: statemachine
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module nonoverlapping_test;
+
+	// Inputs
+	reg clk;
+	reg x;
+
+	// Outputs
+	wire y;
+	wire [1:0] outstate;
+
+	// Instantiate the Unit Under Test (UUT)
+	statemachine uut (
+		.clk(clk), 
+		.x(x), 
+		.y(y), 
+		.outstate(outstate)
+	);
+initial clk=0;
+always #30 clk=~clk;
+	initial begin
+		x = 1;	#100;
+		x = 0;	#100;
+		x = 1;	#100;
+		x = 1;	#100;
+		x = 0;	#100;
+		x = 1;	#100;
+		x = 1;	#100;
+		x = 0;	#100;
+		x = 1;	#100;
+		x = 1;	#100;
+		x = 0;	#100;
+	
+	end
+      
+endmodule
+
