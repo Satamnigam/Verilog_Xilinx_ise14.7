79456a6b2fee284423871db9470b6ae494e9f2ac
Satamnigam <64124488+Satamnigam@users.noreply.github.com>
2021-09-18 01:26:28 +0530

    Add files via upload

---
+++ CLKQUE/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/satyam/Desktop/my codes/CLKQUE/test_qwerty_isim_beh.exe" -prj "C:/Users/satyam/Desktop/my codes/CLKQUE/test_qwerty_beh.prj" "work.test_qwerty" "work.glbl" 
